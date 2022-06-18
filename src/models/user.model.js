8c9a7e15c2e0159f001f046c0cb451b171257c4a
olga <oluciams84@gmail.com>
2022-06-17 19:08:42 -0500

    add bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
@@ -12,6 +13,13 @@ const UserSchema = new Schema({
     type: String,
     require: true,
   }
+});
+
+UserSchema.pre('save', async function(next){
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(this.password, salt)
+  this.password = hash
+  next()
 })
 
 module.exports = mongoose.model('User', UserSchema)
